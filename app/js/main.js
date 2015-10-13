064b1e526cecb91ed84ced44f61195062ffebf58
Luke Griffith <luketgriffith@gmail.com>
2015-10-13 16:37:54 -0400

    finished JS for filling in forms

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,9 @@
 'use strict';
 
-(function () {
-
-  function adhocJsonp(input) {
-    $.ajax({
-      url: 'http://json-data.herokuapp.com/forms',
-      // data: {
-      //   api_key: '',
-      //   keywords: '',
-      //   includes: ''};
-
-      dataType: 'jsonp'
-
-    }).then(function (data) {
-      console.log(data);
-    });
-  }
-})();
+var promise = $.getJSON('http://json-data.herokuapp.com/forms');
+promise.then(function (res) {
+  _.each(res, function (x) {
+    var template = '<div class= \'forms\'>\n        <input type=\'' + x.type + '\' placeholder=\'' + x.label + '\' id=\'' + x.id + '\'>\n        <i class=\'fa ' + x.icon + '\'></i>\n        </div>';
+    $('form').append(template);
+  });
+});
--- src/main.js
+++ src/main.js
@@ -1,23 +1,14 @@
-(function () {
 
 
-
-
-function adhocJsonp (input){
-  $.ajax({
-    url: 'http://json-data.herokuapp.com/forms',
-    // data: {
-    //   api_key: '',
-    //   keywords: '',
-    //   includes: ''};
-    
-    dataType: 'jsonp',
-    
-  }).then(function(data){ 
-     console.log(data);
+var promise = $.getJSON('http://json-data.herokuapp.com/forms');
+  promise.then( function(res){
+    _.each(res, function(x){
+      var template= `<div class= 'forms'>
+        <input type='${ x.type }' placeholder='${ x.label }' id='${ x.id }'>
+        <i class='fa ${ x.icon }'></i>
+        </div>`;
+        $('form').append(template);
   })
-}
+})
 
 
-
-}());
