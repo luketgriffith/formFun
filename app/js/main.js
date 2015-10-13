f8593ec5de86dd6e1ae07e45f8b7a86de0915ea6
Luke Griffith <luketgriffith@gmail.com>
2015-10-13 17:20:01 -0400

    added list styling

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,32 @@
 var promise = $.getJSON('http://json-data.herokuapp.com/forms');
 promise.then(function (res) {
   _.each(res, function (x) {
-    var template = '<div class= \'forms\'>\n        <input type=\'' + x.type + '\' placeholder=\'' + x.label + '\' id=\'' + x.id + '\'>\n        <i class=\'fa ' + x.icon + '\'></i>\n        </div>';
-    $('form').append(template);
+    if (x.label === 'Select Language') {
+      // var language= _.each(x.options, function(y){
+      //   // console.log(y.label);
+      //   return y.label;
+      // })
+      // console.log(language);
+      var template = '<div class=\'selects\'>\n          <select name="select" >\n            <option value="value1">English</option>\n            <option value="value2">Spanish</option>\n            <option value="value3">French</option>\n            <option value="value4">Chinese</option>\n            <option value="value5">Japanese</option> \n          </select>\n          <i class=\'fa ' + x.icon + '\'></i>\n        </div>';
+      $('form').append(template);
+    } else if (x.type === 'text') {
+      var template = '<div class=\'text\'>\n          <input name="text" type=\'' + x.type + '\' placeholder=\'' + x.label + '\' id=\'' + x.id + '\' >\n          <i class=\'fa ' + x.icon + '\'></i>\n        </div>';
+      $('form').append(template);
+    } else if (x.type === 'email') {
+      var template = '<div class=\'email\'>\n          <input name="email" type=\'' + x.type + '\' placeholder=\'' + x.label + '\' id=\'' + x.id + '\' >\n          <i class=\'fa ' + x.icon + '\'></i>\n        </div>';
+      $('form').append(template);
+    } else if (x.type === 'textarea') {
+      var template = '<div class=\'textarea\'>\n          <input name="textarea" type=\'' + x.type + '\' placeholder=\'' + x.label + '\' id=\'' + x.id + '\' >\n          <i class=\'fa ' + x.icon + '\'></i>\n        </div>';
+      $('form').append(template);
+    } else if (x.type === 'tel') {
+      var template = '<div class=\'tel\'>\n          <input name="tel" type=\'' + x.type + '\' placeholder=\'' + x.label + '\' id=\'' + x.id + '\' >\n          <i class=\'fa ' + x.icon + '\'></i>\n          </div>';
+      $('form').append(template);
+    }
   });
 });
+
+// var template= `<div class= 'forms'>
+//   <input type='${ x.type }' placeholder='${ x.label }' id='${ x.id }'>
+//   <i class='fa ${ x.icon }'></i>
+//   </div>`;
+//   $('form').append(template);
--- src/main.js
+++ src/main.js
@@ -3,12 +3,67 @@
 var promise = $.getJSON('http://json-data.herokuapp.com/forms');
   promise.then( function(res){
     _.each(res, function(x){
-      var template= `<div class= 'forms'>
-        <input type='${ x.type }' placeholder='${ x.label }' id='${ x.id }'>
-        <i class='fa ${ x.icon }'></i>
+      if (x.label === 'Select Language'){
+        // var language= _.each(x.options, function(y){
+        //   // console.log(y.label);
+        //   return y.label;
+        // })
+        // console.log(language);
+        var template= `<div class='selects'>
+          <select name="select" >
+            <option value="value1">English</option>
+            <option value="value2">Spanish</option>
+            <option value="value3">French</option>
+            <option value="value4">Chinese</option>
+            <option value="value5">Japanese</option> 
+          </select>
+          <i class='fa ${ x.icon }'></i>
         </div>`;
         $('form').append(template);
-  })
-})
+      } else if (x.type=== 'text'){
+        var template= `<div class='text'>
+          <input name="text" type='${ x.type }' placeholder='${ x.label }' id='${ x.id }' >
+          <i class='fa ${ x.icon }'></i>
+        </div>`;
+        $('form').append(template);
+      } else if (x.type=== 'email'){
+         var template= `<div class='email'>
+          <input name="email" type='${ x.type }' placeholder='${ x.label }' id='${ x.id }' >
+          <i class='fa ${ x.icon }'></i>
+        </div>`;
+        $('form').append(template);
+      } else if(x.type==='textarea'){
+        var template= `<div class='textarea'>
+          <input name="textarea" type='${ x.type }' placeholder='${ x.label }' id='${ x.id }' >
+          <i class='fa ${ x.icon }'></i>
+        </div>`;
+        $('form').append(template);
+      } else if(x.type==='tel'){
+          var template= `<div class='tel'>
+          <input name="tel" type='${ x.type }' placeholder='${ x.label }' id='${ x.id }' >
+          <i class='fa ${ x.icon }'></i>
+          </div>`;
+          $('form').append(template);
+
+      }
+
+
+
+
+      });  
+      });  
+
+
+
+
+
+
+
+      // var template= `<div class= 'forms'>
+      //   <input type='${ x.type }' placeholder='${ x.label }' id='${ x.id }'>
+      //   <i class='fa ${ x.icon }'></i>
+      //   </div>`;
+      //   $('form').append(template);
+
 
 
